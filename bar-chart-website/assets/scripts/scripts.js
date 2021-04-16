ff8a0ad6eb1200dba20fe84d3151514623740da7
David Claveau <daveclaveau@gmail.com>
2021-04-16 23:14:25 +0000

    Added test functionality to add bars to the bar chart div depending on number of values added in an array.

--- bar-chart-website/assets/scripts/scripts.js
+++ bar-chart-website/assets/scripts/scripts.js
@@ -1,9 +1,16 @@
 $(document).ready(function() {
 
-
+  // Grab the string input from data-submit-button
   $( "#data-submit-button" ).click(function() {
-    var dataInput =  $('#data-input').val();
-    $('#bar-chart').html(dataInput);
+    var dataString = $('#data-input').val();
+    var dataArray = dataString.split(',');
+
+    // Convert the string array into a number array
+    for (var data = 0; data < dataArray.length; data++) {
+      dataArray[data] = parseInt(dataArray[data], 10);
+      console.log(typeof dataArray[data]);
+      $( "#bar-chart" ).append('<div class="bar subset-'+ data +'" />');
+    }
 
   });
 
